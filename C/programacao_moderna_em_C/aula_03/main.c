b268878a3110581f63c7748b489402b18fddf22a
Henrique Stocco Machado <henriquestoccomachado@gmail.com>
2025-08-06 07:39:19 -0300

    feat(aula03): ad um exemplo para o tipo size_t e para o moficador register

--- C/programacao_moderna_em_C/aula_03/main.c
+++ C/programacao_moderna_em_C/aula_03/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <limits.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 int main(void)
 {
@@ -11,11 +12,15 @@ int main(void)
     short si = SHRT_MAX; // int de 16 bits
     long li = LONG_MAX; // int de 64 bits (cabe 8 bytes mas ele me mostra apenas 4 bytes, investigarei posteriormente, vi que depende da arquitetura, então cabem apenas 32 bits na arquitetura do meu processador) 
     long long lli = LONG_LONG_MAX; // agora sim os 64 bits
+    size_t len;
+    register int reg_int;
 
     printf("O tamanho de i (int): %zu bytes / %zu bits\n", sizeof(int), sizeof(int) * 8);
     printf("SHORT INT: %llu bytes / %llu bits\n", sizeof si, sizeof si * 8);
     printf("LONG INT: %llu bytes / %llu bits\n", sizeof li, sizeof li * 8);
     printf("LONG LONG INT: %llu bytes / %llu bits\n", sizeof lli, sizeof lli * 8);
+    printf("SIZE_T: %zu bytes\n", sizeof len);
+    printf("REGISTER: %zu bytes\n", sizeof reg_int);
     printf("O valor de simx: %d\n", simx);
     printf("O valor de uimx: %u\n", uimx);
     printf("UIMX em Octal: %o\n", uimx);
